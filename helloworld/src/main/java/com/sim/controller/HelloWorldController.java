bb545619a138ba274c352b71bc5e2dde9320eba0
addurisimhadri <adduriappanna@gmail.com>
2020-05-10 23:01:27 +0530

    sonar lint error message

--- helloworld/src/main/java/com/sim/controller/HelloWorldController.java
+++ helloworld/src/main/java/com/sim/controller/HelloWorldController.java
@@ -1,12 +1,12 @@
 package com.sim.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 	
-	@RequestMapping(value = "/")
+	@GetMapping(path = "/")
 	public String getWelcome() {
 		
 		return "Wlecome to Jenkins";
