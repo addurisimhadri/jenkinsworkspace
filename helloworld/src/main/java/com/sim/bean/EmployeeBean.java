87b1506eee5ea5a68ca2466cf2d4016032bef7d2
addurisimhadri <adduriappanna@gmail.com>
2020-05-15 23:08:29 +0530

    commit

---
+++ helloworld/src/main/java/com/sim/bean/EmployeeBean.java
@@ -0,0 +1,19 @@
+package com.sim.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/*
+@Getter and @Setter annotations for getter and setter methods
+*/
+@Getter
+@Setter
+public class EmployeeBean {
+	
+	private String empId;
+	private String name;
+	private String designation;
+	private double salary;
+
+	
+}
--- helloworld/src/main/java/com/sim/controller/HelloWorldController.java
+++ helloworld/src/main/java/com/sim/controller/HelloWorldController.java
@@ -1,15 +1,31 @@
 package com.sim.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sim.bean.EmployeeBean;
 
 @RestController
 public class HelloWorldController {
 	
-	@GetMapping(path = "/")
-	public String getWelcome() {
+	@GetMapping(path = "/getemployee")
+	public List<EmployeeBean>getEmployee() {
+		ArrayList<EmployeeBean> employeeBeans=new ArrayList<EmployeeBean>();
 		
-		return "Wlecome to Jenkins Hello World";
+		EmployeeBean employeeBean=new EmployeeBean();
+		//employeeBean.s
+		
+		EmployeeBean employeeBean1=new EmployeeBean();
+		
+		
+		employeeBeans.add(employeeBean);
+		employeeBeans.add(employeeBean1);
+
+		
+		return employeeBeans;
 	}
 
 }
